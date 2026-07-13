443a285384f097f80236118769b15f55bc14a97a
Ayrton Andres Carriel Moreira <ycarriel@fiec.espol.edu.ec>
2026-07-13 12:22:27 +0000

    Actualización de métodos

--- poo_proyecto_1p/src/main/java/com/example/Reporte.java
+++ poo_proyecto_1p/src/main/java/com/example/Reporte.java
@@ -1,53 +1,60 @@
 package com.example;
+
 import com.example.enums.*;
 import java.util.ArrayList;
 
-public class Reporte{
+public class Reporte {
     private int totalCompras;
     private int totalEntradas;
     private int totalKits;
     private double montoTotal;
 
-
-    public Reporte(int totalCompras, int totalEntradas, int totalKits, double montoTotal){
-        this.totalCompras=totalCompras;
-        this.totalEntradas=totalEntradas;
-        this.totalKits=totalKits;
-        this.montoTotal=montoTotal;
+    public Reporte(int totalCompras, int totalEntradas, int totalKits, double montoTotal) {
+        this.totalCompras = totalCompras;
+        this.totalEntradas = totalEntradas;
+        this.totalKits = totalKits;
+        this.montoTotal = montoTotal;
     }
 
-    public int getTotalCompras(){
+    public int getTotalCompras() {
         return totalCompras;
     }
-    public int getTotalEntradas(){
-        return totalEntradas;       
+
+    public int getTotalEntradas() {
+        return totalEntradas;
     }
-    public int getTotalKits(){
+
+    public int getTotalKits() {
         return totalKits;
     }
-    public double getMontoTotal(){
+
+    public double getMontoTotal() {
         return montoTotal;
     }
 
-    public void GenerarReporte(ArrayList<Compra> compras){
-        this.totalCompras=compras.size();
-        this.totalEntradas=0;
-        this.totalKits=0;
-        this.montoTotal=0;
+    public void GenerarReporte(ArrayList<Compra> compras) {
+        this.totalCompras = compras.size();
+        this.totalEntradas = 0;
+        this.totalKits = 0;
+        this.montoTotal = 0;
 
-        for(Compra c: compras){
-            if(c.getTipo()==TipoCompra.ENTRADA){
-                totalEntradas+=c.getCantidad();
-            }else if(c.getTipo()==TipoCompra.KIT){
-                totalKits+=c.getCantidad();
+        for (Compra c : compras) {
+            if (c.getTipo() == TipoCompra.ENTRADA) {
+                totalEntradas += c.getCantidad();
+            } else if (c.getTipo() == TipoCompra.KIT) {
+                totalKits += c.getCantidad();
             }
-            montoTotal+=c.getValorPagado();
+            montoTotal += c.getValorPagado();
         }
-        System.out.println("Reporte de compras:\nTotal de compras: "+ 
-        totalCompras+ "\nTotal de entradas: "+
-         totalEntradas+ "\nTotal de kits: "+ totalKits+ 
-         "\nMonto total: "+ montoTotal);
     }
 
+    @Override
+    public String toString() {
+        return "===== REPORTE DE VENTAS =====\n" +
+                "Total de compras: " + totalCompras +
+                "\nEntradas vendidas: " + totalEntradas +
+                "\nKits vendidos: " + totalKits +
+                "\nMonto total: $" + montoTotal;
+    }
 
 }
