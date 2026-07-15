664684077a1e270407c4927556003a902a5e067c
Romina López <raalg004@gmail.com>
2026-07-15 14:48:49 +0000

    correccion FINAL

--- poo_proyecto_1p/src/main/java/com/example/Main.java
+++ poo_proyecto_1p/src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        EnviarCorreo.enviar("ralg7716@gmail.com","Hola","hola");
+        // EnviarCorreo.enviar("ralg7716@gmail.com","Hola","hola");
         Sistema sistema = new Sistema();
         sistema.cargarDatos();
         sistema.iniciarSesion();
